a56c16330f83331bf1029b5f3415920bda7b5cad
Artem Leonidov <leonidovartem85@gmail.com>
2022-07-04 19:03:26 +0300

    memory game on js 1.1

--- game.js
+++ game.js
@@ -18,9 +18,9 @@ cardsArr.sort( function(){
 });
 console.log(cardsArr);
 
-const span = document.querySelector(selectors:"#span");
-const cardsDiv = document.querySelector(selectors:"#cardsDiv");
-const messageDiv = document.querySelector(selectors:"#messageDiv");
+const span = document.querySelector("#span");
+const cardsDiv = document.querySelector("#cardsDiv");
+const messageDiv = document.querySelector("#messageDiv");
 
 
 function createBoard () {
@@ -44,4 +44,40 @@ function checkForMatch(){
     const cards = document.querySelectorAll("img");
     const id1 = cardsChosenArrId [0];
     const id2 = cardsChosenArrId [1];
+
+    if (cardsChosenArr[0] === cardsChosenArr[1] && id1 !== id2) {
+        massageDiv.innerHTML = "Match found!"
+        cards[id1].setAttribute("src", "img/gal.png");
+        cards[id2].setAttribute("src", "img/gal.png");
+
+        cards[id1].removeEventListener("click", flipCard);
+        cards[id2].removeEventListener("click", flipCard);
+
+        nOfOpenedCardsArr.push(cardsChosenArr);
+    }
+    else {
+        cards[id1].setAttribute("src", "img/gal_blank.png");
+        cards[id2].setAttribute("src", "img/gal_blank.png");
+        massageDiv.innerHTML = "Chosen the same card or cards do not match, select another!";
+    }
+
+    cardsChosenArr = [];
+    cardsChosenArrId = [];
+    span.textContent = String(nOfOpenedCardsArr.length);
+
+    if (nOfOpenedCardsArr.length === cardsArr.length / 2) {
+        messageDiv.innerHTML = "You won!"
+    }
+    function flipCsrd() {
+        let cardId = this.getAttribute("id");
+
+        cardsChosenArr.push(cardsArr[cardId].imageName);
+        cardsChosenArrId.push(cardId);
+
+        this.setAttribute("src", cardsArr[cardId].imagePath);
+
+        if (cardsChosenArr.length === 2) {
+            setTimeout(checkForMatch, 1000);
+        }
+    }
 }
